5e0556b13e78c8aa0042d2a3ac752c017429eab5
MarieKelling <mariekelling97@gmail.com>
2017-12-17 18:01:49 -0500

    Updates.

---
+++ Important - Various/zzPersonal Work/C#/HelloWorld.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Windows.Forms;							//Allows you to raise a message box as well as print the results out to the console
+
+public class HellowWorld
+{
+	
+		static void main()
+		{
+			//Write the first mesaage out to the console
+			Console.WriteLine("Hello ");
+			
+			//Write the  second message out to the console
+			//MessageBox.show("World");	
+			
+		}
+	
+	
+	
+	
+	
+}
